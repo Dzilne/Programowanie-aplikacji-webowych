246c77fda908953bb9210e9264adf8bb17ae830b
Dzilne <90028788+Dzilne@users.noreply.github.com>
2023-10-19 12:36:46 +0200

    Update index.mjs

--- 09.10.23/index.mjs
+++ 09.10.23/index.mjs
@@ -1,19 +1,1 @@
-import * as stream from 'stream'
-import * as fs from 'fs'
-
-async function * generatorRandomNumerkuf (){
-    for(let y = 0; y<20 ; y++){
-        let x = Math.floor((Math.random() * 2137) - 420);
-        yield x
-    }
-}
-
-const readable = stream.Readable.from(generatorRandomNumerkuf())
-
-const file = fs.createWriteStream(`random-${Date.now()}.txt`)
-
-readable.on('data', (chunk) =>{
-    file.write(chunk.toString()+'\n')
-})
-
 
